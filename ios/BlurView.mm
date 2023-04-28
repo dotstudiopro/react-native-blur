92ee656cfb7121f197829a5c2cf9fb735e425627
Ketan Sakariya <ketan@dotstudiopro.com>
2023-04-28 11:58:01 +0530

    made blur view compatible with appletv
    made blur view compatible with appletv

--- ios/BlurView.mm
+++ ios/BlurView.mm
@@ -140,7 +140,7 @@ using namespace facebook::react;
     if ([self.blurType isEqual: @"prominent"]) return UIBlurEffectStyleProminent;
   #endif
     
-  #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 130000 /* __IPHONE_13_0 */
+  #if TARGET_OS_IOS && defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 130000 /* __IPHONE_13_0 */
     // Adaptable blur styles
     if ([self.blurType isEqual: @"chromeMaterial"]) return UIBlurEffectStyleSystemChromeMaterial;
     if ([self.blurType isEqual: @"material"]) return UIBlurEffectStyleSystemMaterial;
